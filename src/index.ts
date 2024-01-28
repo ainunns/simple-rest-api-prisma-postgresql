1027594aaa32a42edddac2efca0366ba43e452ee
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2024-01-28 11:29:15 +0700

    feat: create update published status of the blog post route

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,18 @@ app.post("/user", async (req, res) => {
   res.json(result);
 });
 
+app.get("/feed", async (req, res) => {
+  const posts = await prisma.post.findMany({
+    where: {
+      published: true,
+    },
+    include: {
+      author: true,
+    },
+  });
+  res.json(posts);
+});
+
 app.post("/post", async (req, res) => {
   const { title, content, authorEmail } = req.body;
   const result = await prisma.post.create({
@@ -37,18 +49,6 @@ app.post("/post", async (req, res) => {
   res.json(result);
 });
 
-app.get("/feed", async (req, res) => {
-  const posts = await prisma.post.findMany({
-    where: {
-      published: true,
-    },
-    include: {
-      author: true,
-    },
-  });
-  res.json(posts);
-});
-
 app.get("/post/:id", async (req, res) => {
   const { id } = req.params;
   const post = await prisma.post.findUnique({
@@ -57,6 +57,18 @@ app.get("/post/:id", async (req, res) => {
     },
   });
   res.json(post);
+});
+
+app.put("/post/publish/:id", async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.post.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      published: true,
+    },
+  });
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
