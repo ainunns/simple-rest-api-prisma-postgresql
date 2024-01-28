be2e97a69855708aa695e0fb0e01a8dec5e5c323
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2024-01-28 10:20:57 +0700

    feat: create the get users route

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
